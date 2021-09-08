8b61649ae885789f0952be73f6aeac922c308986
Yuuki ゆうき <62160018+CallMeYuuki@users.noreply.github.com>
2021-09-08 09:18:34 -0300

    Only one data constant

--- dataHandler.js
+++ dataHandler.js
@@ -1,4 +1,3 @@
-async function createCmd(Client, guildId) {
     const data = [
         {
             name: 'echo',
@@ -16,27 +15,12 @@ async function createCmd(Client, guildId) {
         }
     ]
 
+async function createCmd(Client, guildId) {
+
     await Client.guilds.cache.get(guildId)?.commands.set(data);
 }
 
 async function globalCmd(Client) {
-    const data = [
-        {
-            name: 'echo',
-            description: 'Replies with sent message',
-            options: [{
-                name: "echo",
-                type: 'STRING',
-                description: 'echo message',
-                require: true
-            }]
-        },
-        {
-            name: 'ping',
-            description: 'Replies with Pong!'
-        }
-    ]
-
 
     await client.application?.commands.create(data);
 }
